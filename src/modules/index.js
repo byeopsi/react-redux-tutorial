5b6b05950856cc29de1ce572789557ca55ae6787
byeopsi <4217609@gmail.com>
2025-12-23 10:49:36 +0900

    리덕스1

---
+++ src/modules/index.js
@@ -0,0 +1,11 @@
+// 리듀서들 하나로 합쳐주기
+import { combineReducers } from "redux";
+import counter from "./counter";
+import todos from "./todos";
+
+const rootReducer = combineReducers({
+  counter,
+  todos,
+});
+
+export default rootReducer;
